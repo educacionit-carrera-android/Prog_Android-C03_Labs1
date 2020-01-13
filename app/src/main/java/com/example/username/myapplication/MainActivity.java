fa826e55eb542082085c5dfb9c6e42d889231cbb
Nicolasd <nicoadiaz93@gmail.com>
2020-01-12 22:29:05 -0300

    Initial commit

---
+++ app/src/main/java/com/example/username/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.username.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
